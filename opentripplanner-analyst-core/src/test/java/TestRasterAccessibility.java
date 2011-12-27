5a0d9ce1a5421e1de579bae6f5aa2079bd8559ba
Andrew Byrd <andrew@fastmail.net>
2011-12-27 23:42:01 +0100

    test writing several image formats via vertexraster

--- opentripplanner-analyst-core/src/test/java/TestRasterAccessibility.java
+++ opentripplanner-analyst-core/src/test/java/TestRasterAccessibility.java
@@ -1,11 +1,7 @@
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -18,40 +14,33 @@ import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.gce.geotiff.GeoTiffWriter;
 import org.geotools.geometry.Envelope2D;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
-import org.onebusaway.gtfs.impl.calendar.CalendarServiceImpl;
-import org.onebusaway.gtfs.model.calendar.CalendarServiceData;
-import org.onebusaway.gtfs.services.calendar.CalendarService;
 import org.opengis.coverage.grid.GridEnvelope;
-import org.opengis.coverage.grid.GridGeometry;
 import org.opengis.geometry.Envelope;
 import org.opentripplanner.analyst.core.TravelTimeImageFunction;
 import org.opentripplanner.analyst.core.VertexRaster;
 import org.opentripplanner.common.IterableLibrary;
 import org.opentripplanner.common.geometry.HashGrid;
-import org.opentripplanner.gtfs.GtfsContext;
-import org.opentripplanner.model.GraphBundle;
 import org.opentripplanner.routing.algorithm.GenericDijkstra;
-import org.opentripplanner.routing.core.OverlayGraph;
 import org.opentripplanner.routing.core.State;
 import org.opentripplanner.routing.core.TraverseOptions;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.impl.GraphServiceImpl;
-import org.opentripplanner.routing.services.GraphService;
 import org.opentripplanner.routing.spt.ShortestPathTree;
 import org.opentripplanner.routing.vertextype.StreetVertex;
 
 public class TestRasterAccessibility extends TestCase {
 
-    private static Graph graph;
-    private static GraphServiceImpl graphService;
-    private static HashGrid<Vertex> hashGrid; 
-    private static TraverseOptions options;
-    private static long tripTime;
+    private Graph graph;
+    private GraphServiceImpl graphService;
+    private HashGrid<Vertex> hashGrid; 
+    private TraverseOptions options;
+    private long tripTime;
 
-    public static void setup() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         // handle this with a graphservice
         File graphFile = new File("/home/syncopate/otp_data/pdx/Graph.obj");
         graph = Graph.load(graphFile, Graph.LoadLevel.FULL);
@@ -75,7 +64,7 @@ public class TestRasterAccessibility extends TestCase {
         options.setTransferTable(graph.getTransferTable());
     }
         
-    public void XtestRaster() throws Exception {
+    public void XtestImageFunction() throws Exception {
         List<Vertex> vertices = new ArrayList<Vertex>(graph.getVertices());
         Collections.shuffle(vertices);
         for (int i = 0; i<3; i++) {
@@ -96,7 +85,6 @@ public class TestRasterAccessibility extends TestCase {
             //LOG.debug(hashGrid.toStringVerbose());
             // GraphRaster gRaster = new GraphRaster(graph, resolution);
             com.vividsolutions.jts.geom.Envelope graphEnvelope = graph.getExtent();
-            BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_BYTE_GRAY);
             Envelope graphRange = new Envelope2D(DefaultGeographicCRS.WGS84, 
                     graphEnvelope.getMinX(),  graphEnvelope.getMinY(), 
                     graphEnvelope.getWidth(), graphEnvelope.getHeight());
@@ -119,28 +107,13 @@ public class TestRasterAccessibility extends TestCase {
             }
             System.out.printf("done writing tiff \n");
         }
-//        new GridCoverageFactory().create(
-//                "name of the coverage",
-//                image,  // a RenderedImage, which is implemented by BufferedImage
-//                null,   // GridGeometry2D gridGeometry,
-//                null,   // GridSampleDimension[] bands,
-//                null,   // GridCoverage[] sources,
-//                null ); // Map<?,?> properties);
-//        
-//        LOG.debug("shuffling vertex list...");
-//        List<Vertex> cv = new ArrayList<Vertex>(g.getVertices());
-//        Collections.shuffle(cv);
-//        int count = 0;
-//        LOG.debug("querying...");
-//        long t0 = System.currentTimeMillis();
-//        long t1 = System.currentTimeMillis();
-//        LOG.debug("{} queries, average query time: {} msec", count, (t1 - t0)/((double)count));
     }
 
     @Test
     public void testVertexRaster() throws Exception {
-        setup();
         VertexRaster raster = new VertexRaster(50);
+        GridCoverage2D coverage = raster.getGridCoverage2D();
+        
         List<Vertex> vertices = new ArrayList<Vertex>(graph.getVertices());
         Collections.shuffle(vertices);
         for (int i = 0; i<10; i++) {
@@ -148,13 +121,34 @@ public class TestRasterAccessibility extends TestCase {
             System.out.println(initialState);
             System.out.printf("finding spt \n");
             ShortestPathTree spt = new GenericDijkstra(options).getShortestPathTree(initialState);
-            BufferedImage image = raster.getImage(spt);
+            raster.generateImage(spt);
+            
+            long t0 = System.currentTimeMillis();
+            System.out.printf("writing png \n");
             File outputfile = new File(String.format("/home/syncopate/out%d.png", i));
-            ImageIO.write(image, "png", outputfile);
-            // try indexed
-            image = raster.getImageIndexed(spt);
-            outputfile = new File(String.format("/home/syncopate/out%didx.png", i));
-            ImageIO.write(image, "png", outputfile);
+            ImageIO.write(raster.getBufferedImage(), "png", outputfile);
+            long t1 = System.currentTimeMillis();
+            System.out.printf("done writing png %dmsec\n", (int)(t1-t0));
+
+//            t0 = System.currentTimeMillis();
+//            System.out.printf("writing jpeg2k \n");
+//            outputfile = new File(String.format("/home/syncopate/out%d.j2k", i));
+//            ImageIO.write(raster.getBufferedImage(), "jpeg2000", outputfile);
+//            t1 = System.currentTimeMillis();
+//            System.out.printf("done writing jpeg2k %dmsec\n", (int)(t1-t0));
+            
+            GeoTiffWriter tiffWriter;
+            File tiff = new File(String.format("/home/syncopate/out%d.tiff", i));
+            t0 = System.currentTimeMillis();
+            System.out.printf("writing geotiff \n");
+            try {
+                tiffWriter = new GeoTiffWriter(tiff);
+                tiffWriter.write(coverage, null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            t1 = System.currentTimeMillis();
+            System.out.printf("done writing geotiff %dmsec\n", (int)(t1-t0));
         }
     }
 }
