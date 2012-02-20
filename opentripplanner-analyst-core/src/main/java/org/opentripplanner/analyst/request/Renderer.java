c920ecc182908c95f7f7b6266b19821bcb8d4c06
Andrew Byrd <andrew@fastmail.net>
2012-02-20 15:43:43 +0100

    move response generation into renderer

--- opentripplanner-analyst-core/src/main/java/org/opentripplanner/analyst/request/Renderer.java
+++ opentripplanner-analyst-core/src/main/java/org/opentripplanner/analyst/request/Renderer.java
@@ -1,17 +1,28 @@
 package org.opentripplanner.analyst.request;
 
 import java.awt.image.BufferedImage;
+import java.io.OutputStream;
 
+import javax.imageio.ImageIO;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
+import org.geotools.coverage.grid.GridCoverage2D;
 import org.opentripplanner.analyst.core.Tile;
+import org.opentripplanner.analyst.rest.parameter.MIMEImageFormat;
 import org.opentripplanner.analyst.rest.utils.TileUtils;
 import org.opentripplanner.routing.spt.ShortestPathTree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Renderer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Renderer.class);
 
     @Autowired
     private TileCache tileCache;
@@ -41,7 +52,39 @@ public class Renderer {
             image = tile.generateImage(sptA, renderRequest);
         }
         
-        return TileUtils.generateStreamingImageResponse(image, renderRequest.format);
+        // TODO: geotiff from gridcoverage
+        //GridCoverage2D gc = tile.getGridCoverage2D(image);
+        return generateStreamingImageResponse(image, renderRequest.format);
     }
     
+    private static Response generateStreamingImageResponse(
+            final BufferedImage image, final MIMEImageFormat format) {
+        
+        if (image == null) {
+            LOG.warn("response image is null");
+        }
+            
+        StreamingOutput streamingOutput = new StreamingOutput() {
+            public void write(OutputStream outStream) {
+                try {
+                    long t0 = System.currentTimeMillis();
+                    ImageIO.write(image, format.type, outStream);
+                    long t1 = System.currentTimeMillis();
+                    LOG.debug("wrote image in {}msec", (int)(t1-t0));
+                } catch (Exception e) {
+                    LOG.error("exception while preparing image : {}", e.getMessage());
+                    throw new WebApplicationException(e);
+                }
+            }
+       };
+
+       CacheControl cc = new CacheControl();
+       cc.setMaxAge(3600);
+       cc.setNoCache(false);
+       return Response.ok(streamingOutput)
+                       .type(format.toString())
+                       .cacheControl(cc)
+                       .build();
+    }
+
 }
