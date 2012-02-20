1a704801ab7059aba27ca7fcad549f6c79672aff
Andrew Byrd <andrew@fastmail.net>
2012-02-20 18:57:17 +0100

    use collection to store valid image types

--- opentripplanner-analyst-core/src/main/java/org/opentripplanner/analyst/rest/parameter/MIMEImageFormat.java
+++ opentripplanner-analyst-core/src/main/java/org/opentripplanner/analyst/rest/parameter/MIMEImageFormat.java
@@ -1,17 +1,22 @@
 package org.opentripplanner.analyst.rest.parameter;
 
+import java.util.Arrays;
+import java.util.Collection;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 public class MIMEImageFormat {
-
+    
+    public static final Collection<String> acceptedTypes =
+        Arrays.asList("png", "gif", "jpeg", "geotiff");
+    
     public final String type;
             
     public MIMEImageFormat(String s) {
         String[] parts = s.split("/");
         if (parts.length == 2 && parts[0].equals("image")) {
-            if (parts[1].equals("png") || parts[1].equals("gif") || parts[1].equals("jpeg") || parts[1].equals("geotiff")) {
+            if (acceptedTypes.contains(parts[1])) {
                 type = parts[1];
             } else {
                 throw new WebApplicationException(Response
