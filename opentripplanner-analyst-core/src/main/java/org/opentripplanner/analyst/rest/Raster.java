1267578dd21d4c98d5391856012d6a6a569a9627
Matt Conway <mattwigway@gmail.com>
2012-02-19 23:07:54 -0800

    initial work on raster endpoint.

--- opentripplanner-analyst-core/src/main/java/org/opentripplanner/analyst/rest/Raster.java
+++ opentripplanner-analyst-core/src/main/java/org/opentripplanner/analyst/rest/Raster.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.Response;
 
 import org.geotools.geometry.Envelope2D;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.geometry.DirectPosition;
 import org.opentripplanner.analyst.core.GeometryIndex;
 import org.opentripplanner.analyst.request.RenderRequest;
 import org.opentripplanner.analyst.request.Renderer;
@@ -42,22 +43,48 @@ public class Raster {
     @GET @Produces("image/geotiff")
     public Response getRaster(
            @QueryParam("x") Float x,  
-           @QueryParam("y") Float y,  
+           @QueryParam("y") Float y,
+           @QueryParam("bbox") Envelope2D bbox,
            @QueryParam("width")  Integer width,  
            @QueryParam("height") Integer height,  
            @QueryParam("time") GregorianCalendar time,
            @QueryParam("crs") // default is WGS84 lat/lon
            @DefaultValue("EPSG:4326") CoordinateReferenceSystem crs ) throws Exception {
         
+        LOG.debug("serving raster request!");
         //com.vividsolutions.jts.geom.Envelope graphEnvelope = graph.getExtent();
 
-        Envelope2D bbox = null; //index.getBoundingEnvelope(srs);
+        LOG.debug("crs = {}", crs.getName());
+        LOG.debug("bbox = {}", bbox);
+        LOG.debug("search time = {}", time);
+
+        // allow height to be left off, figure using constant Cartesian scale
+        // assume height is to y and width is to x
+        if (height == null) {
+            DirectPosition upperCorner = bbox.getUpperCorner();
+            DirectPosition lowerCorner = bbox.getLowerCorner();
+            double resolution = (upperCorner.getOrdinate(0) - lowerCorner.getOrdinate(0))/width;
+            LOG.debug("resolution for request: " + resolution);
+            height = (int) Math.round((
+                upperCorner.getOrdinate(1) - lowerCorner.getOrdinate(1)) * resolution);
+        }
+
+        LOG.debug("calculated height: " + height);
+
+        bbox.setCoordinateReferenceSystem(crs);
+
         TileRequest tileRequest = new TileRequest(bbox, width, height);
-        SPTRequest sptRequest = new SPTRequest(x, y, time);
+        LOG.debug("Tile");
+        SPTRequest sptRequest = new SPTRequest(x, y, time.getTimeInMillis()/1000);
+        LOG.debug("SPT");
+        //  format (GeoTIFF), travel time layer, gray style, not transparent
         RenderRequest renderRequest = new RenderRequest(
-                new MIMEImageFormat("text/geotiff"), 
+                new MIMEImageFormat("image/geotiff"), 
                 Layer.TRAVELTIME, Style.GRAY, false);
 
+        LOG.debug("got this far!");
+
+        // no second SPT, not a difference request
         return renderer.getResponse(tileRequest, sptRequest, null, renderRequest);
         
     }
